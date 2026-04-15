452b78b84e84f233b162686dd4de882645975dc6
diego@byepaper.studio <diego@byepaper.studio>
2026-04-15 00:46:34 -0600

    NEW HOME PAGE

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 import fondoInicio from '../assets/Images/Backgrounds/fondo_inicio.svg'
+import playButton from '../assets/Images/Buttons/Play_Button.png'
 
 function HomePage() {
   return (
@@ -10,9 +13,29 @@ function HomePage() {
         backgroundRepeat: 'no-repeat',
         minHeight: '100vh',
         width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
-
+      <Link to="/login" style={{ marginTop: '20vh' }}>
+        <motion.img
+          src={playButton}
+          alt="Play"
+          style={{
+            width: '220px',
+            borderRadius: '50%',
+            border: '3px solid white',
+            display: 'block',
+          }}
+          animate={{ y: [0, -12, 0] }}
+          transition={{
+            duration: 1,
+            ease: ['easeOut', 'easeIn'],
+            repeat: Infinity,
+          }}
+        />
+      </Link>
     </div>
   )
 }
